e279a7dace3751f41ab83a8a593f6fdadf67ae62
sean-code <john.nganga@student.moringaschool.com>
2022-09-25 21:08:53 +0300

    loop through mapping method

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,25 +1,27 @@
 import React from "react";
 import {useEffect} from 'react;'
 
-let baseUrl = 'http://localhost:8001/transactions'
+let baseUrl = 'http://localhost:8001/transactions';
 
 export default function Transaction() {
   useEffect(()=>{
     fetch(baseUrl)
     .then((data) => {return data.json()}).then((completeData)=>{
       console.log(completeData);
+    completeData.map((values)=>{
+      return (
+        `<tr>
+          <td>${values.date}</td>
+          <td>${values.description}</td>
+          <td>${values.category}</td>
+          <td>${values.amount}</td>
+        </tr>`
+      );
+
+    })
     })
 
+  },  [])
 
-
-  })
-  return (
-    <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-    </tr>
-  );
 }
 
