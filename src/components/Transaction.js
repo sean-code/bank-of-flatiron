2af64c7f8dba48fb8f7e72abbb80fd07a61cf2ec
sean-code <john.nganga@student.moringaschool.com>
2022-09-25 21:01:13 +0300

    fetch from the json server

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,7 +1,18 @@
 import React from "react";
 import {useEffect} from 'react;'
 
+let baseUrl = 'http://localhost:8001/transactions'
+
 export default function Transaction() {
+  useEffect(()=>{
+    fetch(baseUrl)
+    .then((data) => {return data.json()}).then((completeData)=>{
+      console.log(completeData);
+    })
+
+
+
+  })
   return (
     <tr>
       <td>{"your code here..."}</td>
