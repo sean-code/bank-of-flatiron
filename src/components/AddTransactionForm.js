5661c8cb433dd636fcff24f2006ce2a47ae701a1
sean-code <john.nganga@student.moringaschool.com>
2022-09-25 13:07:17 +0300

    Search component handled

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-function AddTransactionForm() {
+
+export default function AddTransactionForm(handleTransaction) {
+  const [formData, setFormData] = useState({
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  })
+  function updateForm(e){
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+  function handleSubmit(e){
+    e.preventDefault()
+    const dataInput = formValidate(formData)
+
+  }
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" onChange={updateForm} value={formData.date}/>
+          <input type="text" name="description" placeholder="Description" onChange={updateForm} value={formData.description}/>
+          <input type="text" name="category" placeholder="Category" onChange={updateForm} value={updateForm.value}/>
+          <input type="number" name="amount" placeholder="Amount" step="0.01" onChange={updateForm} value={formData.amount} />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
@@ -17,5 +32,3 @@ function AddTransactionForm() {
     </div>
   );
 }
-
-export default AddTransactionForm;
