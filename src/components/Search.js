cf6f199258a35675b2b2a8ec5cc63cd84e70eb12
sean-code <john.nganga@student.moringaschool.com>
2022-09-25 12:41:00 +0300

    Search component handled

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,26 @@
 import React from "react";
 
-function Search() {
+
+// function handleData(event){
+//   event.preventDefault
+
+// }
+
+export default function Search({search, setSearch}) {
+  function handleData(et){
+    setSearch(e.target.value)
+  }
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        onChange={(handleData)}
+        value={search}
       />
       <i className="circular search link icon"></i>
     </div>
   );
 }
 
-export default Search;
+
