8c08c6ab6c453d698f0a3d39b8c240cb806b6816
sean-code <john.nganga@student.moringaschool.com>
2022-09-25 22:01:22 +0300

    Add use state and useEffect hooks

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,27 +1,25 @@
-import React from "react";
-import {useEffect} from 'react;'
+import React, {useEffect, useState} from "react";
 
-let baseUrl = 'http://localhost:8001/transactions';
+const baseUrl= "http://localhost:8001/transactions";
 
 export default function Transaction() {
+  const [transact, setTransaction] = useState([''])
   useEffect(()=>{
     fetch(baseUrl)
-    .then((data) => {return data.json()}).then((completeData)=>{
-      console.log(completeData);
-    completeData.map((values)=>{
+      .then((res) => res.json())
+      .then((completedata)=>{
+      console.log(completedata);
+
+     completedata.map((values)=>{
       return (
-        `<tr>
-          <td>${values.date}</td>
-          <td>${values.description}</td>
-          <td>${values.category}</td>
-          <td>${values.amount}</td>
-        </tr>`
-      );
-
+        <tr>
+          <td>{values.date}</td>
+          <td>{values.description}</td>
+          <td>{values.category}</td>
+          <td>{values.amount}</td>
+      </tr>
+      )
     })
     })
-
-  },  [])
-
+  }, [])
 }
-
